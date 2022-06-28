65bad18c420c573dcdc063d19923c069d2d16b3a
TuanDao2002 <tuan09092002@gmail.com>
2022-06-28 14:42:33 +0700

    add upload food image

---
+++ controllers/uploadsController.js
@@ -0,0 +1,20 @@
+const { StatusCodes } = require("http-status-codes");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+
+const uploadFoodImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      folder: "file-upload",
+    }
+  );
+
+  fs.unlinkSync(req.files.image.tempFilePath);
+  return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
+};
+
+module.exports = {
+  uploadFoodImage,
+};
