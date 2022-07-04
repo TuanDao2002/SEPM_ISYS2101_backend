3f52b9005e2e8ef27c39346d2d915c23b0d6e0db
TuanDao2002 <s3877347@rmit.edu.vn>
2022-07-04 23:07:43 +0700

    calculate weigth rating for Review model

--- models/Food.js
+++ models/Food.js
@@ -1,99 +1,92 @@
 const mongoose = require("mongoose");
 
 const FoodSchema = new mongoose.Schema(
-	{
-		foodName: {
-			type: String,
-			required: [true, "Please provide the food name"],
-			minlength: [3, "Length must be greater than 3"],
-			maxlength: [20, "Length must be less than 20"],
-			unique: true,
-		},
+    {
+        foodName: {
+            type: String,
+            required: [true, "Please provide the food name"],
+            minlength: [3, "Length must be greater than 3"],
+            maxlength: [20, "Length must be less than 20"],
+            unique: true,
+        },
 
-		vendor: {
-			type: mongoose.Types.ObjectId,
-			ref: "User",
-			required: true,
-		},
+        vendor: {
+            type: mongoose.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
 
-		location: {
-			type: String,
-			required: [true, "Please provide the location"],
-			minlength: [3, "Length must be greater than 3"],
-			maxlength: [20, "Length must be less than 20"],
-			unique: true,
-		},
+        location: {
+            type: String,
+            required: [true, "Please provide the location"],
+            minlength: [3, "Length must be greater than 3"],
+            maxlength: [20, "Length must be less than 20"],
+            unique: true,
+        },
 
-		price: {
-			type: Number,
-			required: [true, "Please provide the price"],
-		},
+        price: {
+            type: Number,
+            required: [true, "Please provide the price"],
+        },
 
-		category: {
-			type: String,
-			enum: {
-				values: ["Noodle", "Rice", "Soup", "Bread", "Desert"],
-				message: "{VALUE} is not a supported category", // Error message
-			},
-		},
+        category: {
+            type: String,
+            enum: {
+                values: ["Noodle", "Rice", "Soup", "Bread", "Desert"],
+                message: "{VALUE} is not a supported category", // Error message
+            },
+        },
 
-		type: {
-			type: String,
-			enum: {
-				values: ["Breakfast", "Lunch", "Dinner"],
-				message: "{VALUE} is not a supported type of meal", // Error message
-			},
-		},
+        type: {
+            type: String,
+            enum: {
+                values: ["Breakfast", "Lunch", "Dinner"],
+                message: "{VALUE} is not a supported type of meal", // Error message
+            },
+        },
 
-		taste: {
-			type: [String],
-			enum: {
-				values: ["Sweet", "Sour", "Bitter", "Salty"],
-				message: "{VALUE} is not a supported type of taste", // Error message
-			},
-		},
+        taste: {
+            type: [String],
+            enum: {
+                values: ["Sweet", "Sour", "Bitter", "Salty"],
+                message: "{VALUE} is not a supported type of taste", // Error message
+            },
+        },
 
-		prepareTime: {
-			type: Number,
-			min: [1, "Time to prepare must be longer than 0 minute"],
-			max: [59, "Time to prepare must be shorter than 60 minutes"],
-			required: true,
-		},
+        prepareTime: {
+            type: Number,
+            min: [1, "Time to prepare must be longer than 0 minute"],
+            max: [59, "Time to prepare must be shorter than 60 minutes"],
+            required: true,
+        },
 
-		averageRating: {
-			type: Number,
-			default: 0,
-		},
+        averageRating: {
+            type: Number,
+            default: 0,
+        },
 
-		weightRating: {
-			type: Number,
-			default: 0,
-		},
+        weightRating: {
+            type: Number,
+            default: 0,
+        },
 
-		numOfReviews: {
-			type: Number,
-			default: 0,
-		},
+        numOfReviews: {
+            type: Number,
+            default: 0,
+        },
 
-		image: {
-			type: String,
-			default: "image",
-		},
-	},
-	{ timestamps: true }
+        image: {
+            type: String,
+            default: "image",
+        },
+    },
+    { timestamps: true }
 );
 
 FoodSchema.index({ vendor: 1 }, { price: 1, createdAt: -1 });
 
-FoodSchema.virtual("reviews", {
-	ref: "Review",
-	localField: "_id",
-	foreignField: "food",
-	justOne: false,
-});
-
 FoodSchema.pre("remove", async function () {
-	await this.model("Review").deleteMany({ food: this._id });
+    await this.model("Review").deleteMany({ food: this._id });
 });
 
 module.exports = mongoose.model("Food", FoodSchema);
--- models/Review.js
+++ models/Review.js
@@ -1,82 +1,90 @@
 const mongoose = require("mongoose");
 
 const ReviewSchema = new mongoose.Schema(
-	{
-		rating: {
-			type: Number,
-			min: 1,
-			max: 5,
-			required: [true, "Please provide the rating"],
-		},
+    {
+        rating: {
+            type: Number,
+            min: 1,
+            max: 5,
+            required: [true, "Please provide the rating"],
+        },
 
-		title: {
-			type: String,
-			trim: true,
-			required: [true, "Please provide the title"],
-			maxlength: 100,
-		},
+        title: {
+            type: String,
+            trim: true,
+            required: [true, "Please provide the title"],
+            maxlength: 100,
+        },
 
-		comment: {
-			type: String,
-			required: [true, "Please provide the comment"],
-			maxlength: 100,
-		},
+        comment: {
+            type: String,
+            required: [true, "Please provide the comment"],
+            maxlength: 100,
+        },
 
-		user: {
-			type: mongoose.Schema.ObjectId,
-			ref: "User",
-			required: true,
-		},
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: "User",
+            required: true,
+        },
 
-		food: {
-			type: mongoose.Schema.ObjectId,
-			ref: "Food",
-			required: true,
-		},
-	},
-	{ timestamps: true }
+        food: {
+            type: mongoose.Schema.ObjectId,
+            ref: "Food",
+            required: true,
+        },
+    },
+    { timestamps: true }
 );
 
 ReviewSchema.index({ food: 1, user: 1 }, { unique: true });
 
 ReviewSchema.statics.calculateAverageRating = async function (foodId) {
-	const result = await this.aggregate([
-		{ $match: { food: foodId } },
-		{
-			$group: {
-				_id: null,
-				averageRating: { $avg: "$rating" },
-				numOfReviews: { $sum: 1 },
-			},
-		},
-	]);
+    const result = await this.aggregate([
+        { $match: { food: foodId } },
+        {
+            $group: {
+                _id: null,
+                averageRating: { $avg: "$rating" },
+                numOfReviews: { $sum: 1 },
+            },
+        },
+    ]);
 
-	const averageRatingAllFood = await this.model("Food").aggregate({
-		$group: {
-			_id: null,
-			averageRating: { $avg: "$averageRating" }
-		}
-	})
+    const averageRating = result[0]?.averageRating || 0;
+    const numOfReviews = result[0]?.numOfReviews || 0;
 
-	try {
-		await this.model("Food").findOneAndUpdate(
-			{ _id: foodId },
-			{
-				averageRating: Math.ceil(result[0]?.averageRating || 0),
-				numOfReviews: result[0]?.numOfReviews || 0,
-			}
-		);
-	} catch (err) {
-        throw err
-	}
+    const updateFood = await this.model("Food").findOne({ _id: foodId });
+	updateFood.averageRating = averageRating;
+	updateFood.numOfReviews = numOfReviews;
+
+	await updateFood.save();
+
+    const resultsFromAllFood = await this.model("Food").aggregate([
+        {
+            $group: {
+                _id: null,
+                averageRating: { $avg: "$averageRating" },
+            },
+        },
+    ]);
+
+    const averageRatingAllFood = resultsFromAllFood[0]?.averageRating || 0;
+    const acceptedNumOfReviews = 10;
+    const weightRating =
+        (averageRating * numOfReviews + averageRatingAllFood * acceptedNumOfReviews) /
+        (numOfReviews + acceptedNumOfReviews);
+
+	updateFood.weightRating = weightRating;
+	await updateFood.save();
 };
 
 ReviewSchema.post("save", async function () {
-	await this.constructor.calculateAverageRating(this.food);
+    await this.constructor.calculateAverageRating(this.food);
 });
 
 ReviewSchema.post("remove", async function () {
-	await this.constructor.calculateAverageRating(this.food);
+    await this.constructor.calculateAverageRating(this.food);
 });
 
 module.exports = mongoose.model("Review", ReviewSchema);
