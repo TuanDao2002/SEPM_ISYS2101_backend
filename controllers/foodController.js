23e6655c8b6cd0cce17d69c14ca7825c83ef9c68
TuanDao2002 <tuan09092002@gmail.com>
2022-07-04 10:17:32 +0700

    refactor code

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -5,61 +5,63 @@ const Food = require("../models/Food");
 const User = require("../models/User");
 
 const getAllFood = async (req, res) => {
-    let { category, vendor, taste, minPrice, maxPrice, type, next_cursor } =
-        req.query;
-    const queryObject = {};
+	let { category, vendor, taste, minPrice, maxPrice, type, next_cursor } =
+		req.query;
+	const queryObject = {};
 
-    if (category) {
-        queryObject.category = category;
-    }
+	if (category) {
+		queryObject.category = category;
+	}
 
-    if (vendor) {
-        const findVendor = await User.findOne({ username: vendor });
-        if (!findVendor) {
-            throw new CustomError.BadRequestError("This vendor does not exist");
-        }
+	if (vendor) {
+		const findVendor = await User.findOne({ username: vendor });
+		if (!findVendor) {
+			throw new CustomError.BadRequestError("This vendor does not exist");
+		}
 
-        queryObject.vendor = findVendor._id;
-    }
+		queryObject.vendor = findVendor._id;
+	}
 
-    if (taste) {
-        queryObject.taste = {
-            $in: taste.split(","),
-        };
-    }
+	if (taste) {
+		queryObject.taste = {
+			$in: taste.split(","),
+		};
+	}
 
-    if (minPrice) {
-        queryObject.price = { $gte: Number(minPrice) };
-    }
+	if (minPrice) {
+		queryObject.price = { $gte: Number(minPrice) };
+	}
 
-    if (maxPrice) {
-        queryObject.price = { ...queryObject.price, $lte: Number(maxPrice) }; // use spread operator to NOT override the previous field
-    }
+	if (maxPrice) {
+		queryObject.price = { ...queryObject.price, $lte: Number(maxPrice) }; // use spread operator to NOT override the previous field
+	}
 
-    if (type) {
-        queryObject.type = type;
-    } else {
-        // if meal type is not specified
-        const now = new Date();
-        const hours = now.getHours(); // get the hour of the day based on local time
-        const type = hours < 11 ? "Breakfast" : hours < 16 ? "Lunch" : "Dinner"; // return the meal type based on the hour of the day
-        queryObject.type = type;
-    }
+	if (type) {
+		queryObject.type = type;
+	} else {
+		// if meal type is not specified
+		const now = new Date();
+		const hours = now.getHours(); // get the hour of the day based on local time
+		const type = hours < 11 ? "Breakfast" : hours < 16 ? "Lunch" : "Dinner"; // return the meal type based on the hour of the day
+		queryObject.type = type;
+	}
 
-    if (next_cursor) {
-        const [price, createdAt, _id] = Buffer.from(next_cursor, "base64")
-            .toString("ascii")
-            .split("_");
+	if (next_cursor) {
+		const [price, createdAt, _id] = Buffer.from(next_cursor, "base64")
+			.toString("ascii")
+			.split("_");
 
-        queryObject.$or = [
-            { price: { $gt: price } },
-            { price: price, createdAt: { $lt: createdAt } },
-            { createdAt: createdAt, _id: { $lt: _id } }
-        ];
-    }
+		queryObject.$or = [
+			{ price: { $gt: price } },
+			{ price: price, createdAt: { $lt: createdAt } },
+			{ createdAt: createdAt, _id: { $lt: _id } },
+		];
+	}
 
-    const resultsLimitPerLoading = 4;
+	const resultsLimitPerLoading = 4
+    ;
 
+	/*
     try {
         await Food.find(queryObject)
             .select("foodName price vendor image taste location createdAt")
@@ -91,90 +93,118 @@ const getAllFood = async (req, res) => {
     } catch (err) {
         throw err;
     }
+    */
+
+	let foods = Food.find(queryObject)
+		.select("foodName price vendor image taste location createdAt")
+		.populate({
+			path: "vendor",
+			select: "-_id username", // select username and not include _id
+		});
+
+	foods = foods.sort("price -createdAt");
+	foods = foods.limit(resultsLimitPerLoading);
+	const results = await foods;
+
+	const count = await Food.countDocuments(queryObject);
+	const remainingResults = count - results.length;
+	next_cursor = null;
+	if (results.length !== count) {
+		const lastResult = results[results.length - 1];
+		next_cursor = Buffer.from(
+			lastResult.price + "_" + lastResult.createdAt + "_" + lastResult._id
+		).toString("base64");
+	}
+
+	res.status(StatusCodes.OK).json({
+		results,
+		remainingResults,
+		next_cursor,
+	});
 };
 
 const getFood = async (req, res) => {
-    const { id: foodId } = req.params;
-    const food = await Food.findOne({ _id: foodId });
+	const { id: foodId } = req.params;
+	const food = await Food.findOne({ _id: foodId });
 
-    if (!food) {
-        throw new CustomError.NotFoundError(
-            `Food with id ${foodId} does not exist`
-        );
-    }
-    res.status(StatusCodes.OK).json({ food });
+	if (!food) {
+		throw new CustomError.NotFoundError(
+			`Food with id ${foodId} does not exist`
+		);
+	}
+	res.status(StatusCodes.OK).json({ food });
 };
 
 const createFood = async (req, res) => {
-    const {
-        body: { foodName },
-        user: { userId },
-    } = req;
+	const {
+		body: { foodName },
+		user: { userId },
+	} = req;
 
-    const duplicateFood = await Food.findOne({
-        foodName: { $regex: `^${foodName}$`, $options: "i" }, // find duplicate food with case insensitive
-        vendor: userId,
-    });
+	const duplicateFood = await Food.findOne({
+		foodName: { $regex: `^${foodName}$`, $options: "i" }, // find duplicate food with case insensitive
+		vendor: userId,
+	});
 
-    if (duplicateFood) {
-        throw new CustomError.BadRequestError(
-            "This food already exists in this vendor"
-        );
-    }
+	if (duplicateFood) {
+		throw new CustomError.BadRequestError(
+			"This food already exists in this vendor"
+		);
+	}
 
-    req.body.vendor = userId;
+	req.body.vendor = userId;
 
-    const food = await Food.create(req.body);
-    res.status(StatusCodes.OK).json({ food });
+	const food = await Food.create(req.body);
+	res.status(StatusCodes.OK).json({ food });
 };
 
 const updateFood = async (req, res) => {
-    const {
-        params: { id: foodId },
-        user: { userId },
-    } = req;
+	const {
+		params: { id: foodId },
+		user: { userId },
+	} = req;
 
-    const food = await Food.findOneAndUpdate(
-        { _id: foodId, vendor: userId },
-        req.body,
-        {
-            new: true, // always return the new updated object
-            runValidators: true, // always validate the attributes of the object
-            useFindAndModify: false, // not show warning message
-        }
-    );
+	const food = await Food.findOneAndUpdate(
+		{ _id: foodId, vendor: userId },
+		req.body,
+		{
+			new: true, // always return the new updated object
+			runValidators: true, // always validate the attributes of the object
+			useFindAndModify: false, // not show warning message
+		}
+	);
 
-    if (!food) {
-        throw new CustomError.BadRequestError(
-            `Food with id ${foodId} does not exist or this vendor does not own this food`
-        );
-    }
-    res.status(StatusCodes.OK).json({ food });
+	if (!food) {
+		throw new CustomError.BadRequestError(
+			`Food with id ${foodId} does not exist or this vendor does not own this food`
+		);
+	}
+	res.status(StatusCodes.OK).json({ food });
 };
 
 const deleteFood = async (req, res) => {
-    const {
-        params: { id: foodId },
-        user: { userId },
-    } = req;
+	const {
+		params: { id: foodId },
+		user: { userId },
+	} = req;
 
-    const food = await Food.findOne({ _id: foodId, vendor: userId });
+	const food = await Food.findOne({ _id: foodId, vendor: userId });
 
-    if (!food) {
-        throw new CustomError.BadRequestError(
-            `Food with id ${foodId} does not exist or this vendor does not own this food`
-        );
-    }
+	if (!food) {
+		throw new CustomError.BadRequestError(
+			`Food with id ${foodId} does not exist or this vendor does not own this food`
+		);
+	}
 
-    await food.remove();
+	await food.remove();
 
-    res.status(200).json({ msg: "Success! Food deleted" });
+	res.status(200).json({ msg: "Success! Food deleted" });
 };
 
 module.exports = {
-    getAllFood,
-    getFood,
-    createFood,
-    updateFood,
-    deleteFood,
+	getAllFood,
+	getFood,
+	createFood,
+	updateFood,
+	deleteFood,
 };
--- models/Food.js
+++ models/Food.js
@@ -1,73 +1,73 @@
 const mongoose = require("mongoose");
 
 const FoodSchema = new mongoose.Schema(
-    {
-        foodName: {
-            type: String,
-            required: [true, "Please provide the food name"],
-            minlength: [3, "Length must be greater than 3"],
-            maxlength: [20, "Length must be less than 20"],
-            unique: true
-        },
+	{
+		foodName: {
+			type: String,
+			required: [true, "Please provide the food name"],
+			minlength: [3, "Length must be greater than 3"],
+			maxlength: [20, "Length must be less than 20"],
+			unique: true,
+		},
 
-        vendor: {
-            type: mongoose.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
+		vendor: {
+			type: mongoose.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
 
-        location: {
-            type: String,
-            required: [true, "Please provide the location"],
-            minlength: [3, "Length must be greater than 3"],
-            maxlength: [20, "Length must be less than 20"],
-            unique: true
-        },
+		location: {
+			type: String,
+			required: [true, "Please provide the location"],
+			minlength: [3, "Length must be greater than 3"],
+			maxlength: [20, "Length must be less than 20"],
+			unique: true,
+		},
 
-        price: {
-            type: Number,
-            required: [true, "Please provide the price"],
-        },
+		price: {
+			type: Number,
+			required: [true, "Please provide the price"],
+		},
 
-        category: {
-            type: String,
-            enum: {
-                values: ["Noodle", "Rice", "Soup", "Bread", "Desert"],
-                message: "{VALUE} is not a supported category", // Error message
-            },
-        },
+		category: {
+			type: String,
+			enum: {
+				values: ["Noodle", "Rice", "Soup", "Bread", "Desert"],
+				message: "{VALUE} is not a supported category", // Error message
+			},
+		},
 
-        type: {
-            type: String,
-            enum: {
-                values: ["Breakfast", "Lunch", "Dinner"],
-                message: "{VALUE} is not a supported type of meal", // Error message
-            },
-        },
+		type: {
+			type: String,
+			enum: {
+				values: ["Breakfast", "Lunch", "Dinner"],
+				message: "{VALUE} is not a supported type of meal", // Error message
+			},
+		},
 
-        taste: {
-            type: [String],
-            enum: {
-                values: ["Sweet", "Sour", "Bitter", "Salty"],
-                message: "{VALUE} is not a supported type of taste", // Error message
-            },
-        },
+		taste: {
+			type: [String],
+			enum: {
+				values: ["Sweet", "Sour", "Bitter", "Salty"],
+				message: "{VALUE} is not a supported type of taste", // Error message
+			},
+		},
 
-        prepareTime: {
-            type: Number,
-            min: [1, "Time to prepare must be longer than 0 minute"],
-            max: [59, "Time to prepare must be shorter than 60 minutes"],
-            required: true
-        },
+		prepareTime: {
+			type: Number,
+			min: [1, "Time to prepare must be longer than 0 minute"],
+			max: [59, "Time to prepare must be shorter than 60 minutes"],
+			required: true,
+		},
 
-        image: {
-            type: String,
-            default: "image",
-        },
-    },
-    { timestamps: true }
+		image: {
+			type: String,
+			default: "image",
+		},
+	},
+	{ timestamps: true }
 );
 
-FoodSchema.index({ vendor: 1, price: 1, createdAt: -1 });
+FoodSchema.index({ vendor: 1 }, { price: 1, createdAt: -1 });
 
 module.exports = mongoose.model("Food", FoodSchema);
