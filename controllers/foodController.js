15f3d94e4d7790ba43b6f3f58b02443fd6d9315d
TuanDao2002 <s3877347@rmit.edu.vn>
2022-07-31 10:23:09 +0700

    add _id to cursor and modify

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -4,309 +4,315 @@ const CustomError = require("../errors");
 const Food = require("../models/Food");
 const User = require("../models/User");
 
-const { createAllAttributeSets, findSimilar, createAllFoodProfiles, recommedFoodsForStudent } = require("../computation/index");
+const {
+    createAllAttributeSets,
+    findSimilar,
+    createAllFoodProfiles,
+    recommedFoodsForStudent,
+} = require("../computation/index");
 
 // regex check if there are any tag
 const regex = /<.*>/g;
 
 const getAllFood = async (req, res) => {
-	let {
-		foodName,
-		category,
-		vendor,
-		taste,
-		minPrice,
-		maxPrice,
-		type,
-		next_cursor,
-	} = req.query;
+    let {
+        foodName,
+        category,
+        vendor,
+        taste,
+        minPrice,
+        maxPrice,
+        type,
+        next_cursor,
+    } = req.query;
 
-	const queryObject = {};
-	if (foodName) {
-		queryObject.foodName = { $regex: `${foodName}`, $options: "i" };
-	}
+    const queryObject = {};
+    if (foodName) {
+        queryObject.foodName = { $regex: `${foodName}`, $options: "i" };
+    }
 
-	if (category) {
-		queryObject.category = category;
-	}
+    if (category) {
+        queryObject.category = category;
+    }
 
-	if (vendor) {
-		const findVendor = await User.findOne({ username: vendor });
-		if (!findVendor) {
-			throw new CustomError.BadRequestError("This vendor does not exist");
-		}
+    if (vendor) {
+        const findVendor = await User.findOne({ username: vendor });
+        if (!findVendor) {
+            throw new CustomError.BadRequestError("This vendor does not exist");
+        }
 
-		queryObject.vendor = findVendor._id;
-	}
+        queryObject.vendor = findVendor._id;
+    }
 
-	if (taste) {
-		queryObject.taste = {
-			$in: taste.split(","),
-		};
-	}
+    if (taste) {
+        queryObject.taste = {
+            $in: taste.split(","),
+        };
+    }
 
-	if (minPrice) {
-		queryObject.price = { $gte: Number(minPrice) };
-	}
+    if (minPrice) {
+        queryObject.price = { $gte: Number(minPrice) };
+    }
 
-	if (maxPrice) {
-		queryObject.price = { ...queryObject.price, $lte: Number(maxPrice) }; // use spread operator to NOT override the previous field
-	}
+    if (maxPrice) {
+        queryObject.price = { ...queryObject.price, $lte: Number(maxPrice) }; // use spread operator to NOT override the previous field
+    }
 
-	if (type) {
-		queryObject.type = type;
-	} else {
-		// if meal type is not specified
-		const now = new Date();
-		const hours = now.getHours(); // get the hour of the day based on local time
-		const type = hours < 11 ? "Breakfast" : hours < 16 ? "Lunch" : "Dinner"; // return the meal type based on the hour of the day
-		queryObject.type = type;
-	}
+    if (type) {
+        queryObject.type = type;
+    } else {
+        // if meal type is not specified
+        const now = new Date();
+        const hours = now.getHours(); // get the hour of the day based on local time
+        const type = hours < 11 ? "Breakfast" : hours < 16 ? "Lunch" : "Dinner"; // return the meal type based on the hour of the day
+        queryObject.type = type;
+    }
 
-	if (next_cursor) {
-		const [weightRating, price, createdAt] = Buffer.from(
-			next_cursor,
-			"base64"
-		)
-			.toString("ascii")
-			.split("_");
+    if (next_cursor) {
+        const [weightRating, price, createdAt, _id] = Buffer.from(
+            next_cursor,
+            "base64"
+        )
+            .toString("ascii")
+            .split("_");
 
-		queryObject.$or = [
-			{ weightRating: { $lt: weightRating } },
-			{ weightRating: weightRating, price: { $gt: price } },
-			{ price: price, createdAt: { $lt: createdAt } }
-		];
-	}
+        queryObject.$or = [
+            { weightRating: { $lt: weightRating } },
+            { weightRating: weightRating, price: { $gt: price } },
+            { price: price, createdAt: { $lt: createdAt } },
+            { createdAt: createdAt, _id: { $ne: _id } },
+        ];
+    }
 
-	const resultsLimitPerLoading = 5;
-	let foods = Food.find(queryObject)
-		.select(
-			"foodName price vendor averageRating weightRating image createdAt"
-		)
-		.populate({
-			path: "vendor",
-			select: "-_id username", // select username and not include _id
-		});
-	foods = foods.sort("-weightRating price -createdAt");
-	foods = foods.limit(resultsLimitPerLoading);
-	const results = await foods;
+    const resultsLimitPerLoading = 5;
+    let foods = Food.find(queryObject)
+        .select(
+            "foodName price vendor averageRating weightRating image createdAt"
+        )
+        .populate({
+            path: "vendor",
+            select: "-_id username", // select username and not include _id
+        });
+    foods = foods.sort("-weightRating price -createdAt");
+    foods = foods.limit(resultsLimitPerLoading);
+    const results = await foods;
 
-	const count = await Food.countDocuments(queryObject);
-	const remainingResults = count - results.length;
-	next_cursor = null;
-	if (results.length !== count) {
-		const lastResult = results[results.length - 1];
-		next_cursor = Buffer.from(
-			lastResult.weightRating +
-			"_" +
-			lastResult.price +
-			"_" +
-			lastResult.createdAt.toISOString()
-		).toString("base64");
-	}
+    const count = await Food.countDocuments(queryObject);
+    const remainingResults = count - results.length;
+    next_cursor = null;
+    if (results.length !== count) {
+        const lastResult = results[results.length - 1];
+        next_cursor = Buffer.from(
+            lastResult.weightRating +
+                "_" +
+                lastResult.price +
+                "_" +
+                lastResult.createdAt.toISOString() +
+                "_" +
+                lastResult._id
+        ).toString("base64");
+    }
 
-	res.status(StatusCodes.OK).json({
-		results,
-		remainingResults,
-		next_cursor,
-	});
+    res.status(StatusCodes.OK).json({
+        results,
+        remainingResults,
+        next_cursor,
+    });
 };
 
 const getFood = async (req, res) => {
-	const { id: foodId } = req.params;
-	const food = await Food.findOne({ _id: foodId })
-		.populate({
-			path: "vendor",
-			select: "-_id username", // select username and not include _id
-		})
-		.populate({
-			path: "similarOnes",
-			select:
-				"foodName price vendor averageRating weightRating image createdAt",
-			populate: {
-				path: "vendor",
-				select: "-_id username", // select username and not include _id
-			},
-		});
+    const { id: foodId } = req.params;
+    const food = await Food.findOne({ _id: foodId })
+        .populate({
+            path: "vendor",
+            select: "-_id username", // select username and not include _id
+        })
+        .populate({
+            path: "similarOnes",
+            select: "foodName price vendor averageRating weightRating image createdAt",
+            populate: {
+                path: "vendor",
+                select: "-_id username", // select username and not include _id
+            },
+        });
 
-	if (!food) {
-		throw new CustomError.NotFoundError(
-			`Food with id ${foodId} does not exist`
-		);
-	}
-	res.status(StatusCodes.OK).json({ food });
+    if (!food) {
+        throw new CustomError.NotFoundError(
+            `Food with id ${foodId} does not exist`
+        );
+    }
+    res.status(StatusCodes.OK).json({ food });
 };
 
 const createFood = async (req, res) => {
-	const {
-		body: {
-			foodName,
-			location,
-			price,
-			category,
-			type,
-			taste,
-			prepareTime,
-			image,
-		},
-		user: { userId },
-	} = req;
+    const {
+        body: {
+            foodName,
+            location,
+            price,
+            category,
+            type,
+            taste,
+            prepareTime,
+            image,
+        },
+        user: { userId },
+    } = req;
 
-	if (foodName.match(regex) || location.match(regex)) {
-		throw new CustomError.BadRequestError(
-			"This food name or location must not have strange characters"
-		);
-	}
+    if (foodName.match(regex) || location.match(regex)) {
+        throw new CustomError.BadRequestError(
+            "This food name or location must not have strange characters"
+        );
+    }
 
-	const duplicateFood = await Food.findOne({
-		foodName: { $regex: `^${foodName}$`, $options: "i" }, // find duplicate food with case insensitive
-		vendor: userId,
-	});
+    const duplicateFood = await Food.findOne({
+        foodName: { $regex: `^${foodName}$`, $options: "i" }, // find duplicate food with case insensitive
+        vendor: userId,
+    });
 
-	if (duplicateFood) {
-		throw new CustomError.BadRequestError(
-			"This food already exists in this vendor"
-		);
-	}
+    if (duplicateFood) {
+        throw new CustomError.BadRequestError(
+            "This food already exists in this vendor"
+        );
+    }
 
-	const newFood = {
-		foodName,
-		location,
-		price,
-		category,
-		type,
-		taste,
-		prepareTime,
-		image,
-		vendor: userId,
-	};
+    const newFood = {
+        foodName,
+        location,
+        price,
+        category,
+        type,
+        taste,
+        prepareTime,
+        image,
+        vendor: userId,
+    };
 
-	const food = await Food.create(newFood);
-	res.status(StatusCodes.OK).json({ food });
+    const food = await Food.create(newFood);
+    res.status(StatusCodes.OK).json({ food });
 
-	const allFoods = await Food.find();
-	const allAttributeSets = await createAllAttributeSets(allFoods);
-	for (eachFood of allFoods) {
-		findSimilar(eachFood, allAttributeSets);
-	}
+    const allFoods = await Food.find();
+    const allAttributeSets = await createAllAttributeSets(allFoods);
+    for (eachFood of allFoods) {
+        findSimilar(eachFood, allAttributeSets);
+    }
 
-	const profiles = await createAllFoodProfiles(allFoods);
-	const allStudents = await User.find({ role: "student" });
-	for (eachStudent of allStudents) {
-		recommedFoodsForStudent(eachStudent._id, profiles);
-	}
+    const profiles = await createAllFoodProfiles(allFoods);
+    const allStudents = await User.find({ role: "student" });
+    for (eachStudent of allStudents) {
+        recommedFoodsForStudent(eachStudent._id, profiles);
+    }
 };
 
 const updateFood = async (req, res) => {
-	const {
-		params: { id: foodId },
-		body: {
-			foodName,
-			location,
-			price,
-			category,
-			type,
-			taste,
-			prepareTime,
-			image,
-		},
-		user: { userId },
-	} = req;
+    const {
+        params: { id: foodId },
+        body: {
+            foodName,
+            location,
+            price,
+            category,
+            type,
+            taste,
+            prepareTime,
+            image,
+        },
+        user: { userId },
+    } = req;
 
-	if (foodName.match(regex) || location.match(regex)) {
-		throw new CustomError.BadRequestError(
-			"This food name or location must not have strange characters"
-		);
-	}
+    if (foodName.match(regex) || location.match(regex)) {
+        throw new CustomError.BadRequestError(
+            "This food name or location must not have strange characters"
+        );
+    }
 
-	const duplicateFood = await Food.findOne({
-		foodName: { $regex: `^${foodName}$`, $options: "i" }, // find duplicate food with case insensitive
-		vendor: userId,
-	});
+    const duplicateFood = await Food.findOne({
+        foodName: { $regex: `^${foodName}$`, $options: "i" }, // find duplicate food with case insensitive
+        vendor: userId,
+    });
 
-	if (duplicateFood && duplicateFood._id != foodId) {
-		throw new CustomError.BadRequestError(
-			"This food already exists in this vendor"
-		);
-	}
+    if (duplicateFood && duplicateFood._id != foodId) {
+        throw new CustomError.BadRequestError(
+            "This food already exists in this vendor"
+        );
+    }
 
-	const food = await Food.findOne({ _id: foodId, vendor: userId });
-	if (!food) {
-		throw new CustomError.BadRequestError(
-			`Food with id ${foodId} does not exist or this vendor does not own this food`
-		);
-	}
+    const food = await Food.findOne({ _id: foodId, vendor: userId });
+    if (!food) {
+        throw new CustomError.BadRequestError(
+            `Food with id ${foodId} does not exist or this vendor does not own this food`
+        );
+    }
 
-	food.foodName = foodName;
-	food.location = location;
-	food.price = price;
-	food.category = category;
-	food.type = type;
-	food.taste = taste;
-	food.prepareTime = prepareTime;
-	food.image = image;
-	await food.save();
-	res.status(StatusCodes.OK).json({ food });
+    food.foodName = foodName;
+    food.location = location;
+    food.price = price;
+    food.category = category;
+    food.type = type;
+    food.taste = taste;
+    food.prepareTime = prepareTime;
+    food.image = image;
+    await food.save();
+    res.status(StatusCodes.OK).json({ food });
 
-	const allFoods = await Food.find();
-	const allAttributeSets = await createAllAttributeSets(allFoods);
-	for (eachFood of allFoods) {
-		findSimilar(eachFood, allAttributeSets);
-	}
+    const allFoods = await Food.find();
+    const allAttributeSets = await createAllAttributeSets(allFoods);
+    for (eachFood of allFoods) {
+        findSimilar(eachFood, allAttributeSets);
+    }
 
-	const profiles = await createAllFoodProfiles(allFoods);
-	const allStudents = await User.find({ role: "student" });
-	for (eachStudent of allStudents) {
-		recommedFoodsForStudent(eachStudent._id, profiles);
-	}
+    const profiles = await createAllFoodProfiles(allFoods);
+    const allStudents = await User.find({ role: "student" });
+    for (eachStudent of allStudents) {
+        recommedFoodsForStudent(eachStudent._id, profiles);
+    }
 };
 
 const deleteFood = async (req, res) => {
-	const {
-		params: { id: foodId },
-		user: { userId },
-	} = req;
+    const {
+        params: { id: foodId },
+        user: { userId },
+    } = req;
 
-	const food = await Food.findOne({ _id: foodId, vendor: userId });
+    const food = await Food.findOne({ _id: foodId, vendor: userId });
 
-	if (!food) {
-		throw new CustomError.BadRequestError(
-			`Food with id ${foodId} does not exist or this vendor does not own this food`
-		);
-	}
+    if (!food) {
+        throw new CustomError.BadRequestError(
+            `Food with id ${foodId} does not exist or this vendor does not own this food`
+        );
+    }
 
-	await food.remove();
-	res.status(200).json({ msg: "Success! Food deleted" });
+    await food.remove();
+    res.status(200).json({ msg: "Success! Food deleted" });
 
-	const allFoods = await Food.find();
-	const allAttributeSets = await createAllAttributeSets(allFoods);
-	for (eachFood of allFoods) {
-		findSimilar(eachFood, allAttributeSets);
-	}
+    const allFoods = await Food.find();
+    const allAttributeSets = await createAllAttributeSets(allFoods);
+    for (eachFood of allFoods) {
+        findSimilar(eachFood, allAttributeSets);
+    }
 
-	const profiles = await createAllFoodProfiles(allFoods);
-	const allStudents = await User.find({ role: "student" });
-	for (eachStudent of allStudents) {
-		recommedFoodsForStudent(eachStudent._id, profiles);
-	}
+    const profiles = await createAllFoodProfiles(allFoods);
+    const allStudents = await User.find({ role: "student" });
+    for (eachStudent of allStudents) {
+        recommedFoodsForStudent(eachStudent._id, profiles);
+    }
 };
 
 const recommendFoods = async (req, res) => {
-	const user = await User.findOne({ _id: req.user.userId })
-		.select("recommendFoods")
-		.populate({
-			path: "recommendFoods",
-			select:
-				"foodName price vendor averageRating weightRating image createdAt",
-		});
-	res.status(200).json({ user });
+    const user = await User.findOne({ _id: req.user.userId })
+        .select("recommendFoods")
+        .populate({
+            path: "recommendFoods",
+            select: "foodName price vendor averageRating weightRating image createdAt",
+        });
+    res.status(200).json({ user });
 };
 
 module.exports = {
-	getAllFood,
-	getFood,
-	createFood,
-	updateFood,
-	deleteFood,
-	recommendFoods,
+    getAllFood,
+    getFood,
+    createFood,
+    updateFood,
+    deleteFood,
+    recommendFoods,
 };
--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -8,137 +8,142 @@ const Food = require("../models/Food");
 const regex = /<.*>/g;
 
 const createReview = async (req, res) => {
-	let { food: foodId, rating, title, comment } = req.body;
+    let { food: foodId, rating, title, comment } = req.body;
 
-	if (title.match(regex) || comment.match(regex)) {
-		throw new CustomError.BadRequestError(
-			"This title or comment must not have strange characters"
-		);
-	}
+    if (title.match(regex) || comment.match(regex)) {
+        throw new CustomError.BadRequestError(
+            "This title or comment must not have strange characters"
+        );
+    }
 
-	const isValidFood = await Food.findOne({ _id: foodId });
+    const isValidFood = await Food.findOne({ _id: foodId });
 
-	if (!isValidFood) {
-		throw new CustomError.NotFoundError(`No food with id : ${foodId}`);
-	}
+    if (!isValidFood) {
+        throw new CustomError.NotFoundError(`No food with id : ${foodId}`);
+    }
 
-	const alreadySubmitted = await Review.findOne({
-		food: foodId,
-		user: req.user.userId,
-	});
+    const alreadySubmitted = await Review.findOne({
+        food: foodId,
+        user: req.user.userId,
+    });
 
-	if (alreadySubmitted) {
-		throw new CustomError.BadRequestError(
-			"Already submitted review for this food"
-		);
-	}
+    if (alreadySubmitted) {
+        throw new CustomError.BadRequestError(
+            "Already submitted review for this food"
+        );
+    }
 
-	// escape tags to prevent XSS
-	const newReview = {
-		rating,
-		title,
-		comment,
-		user: req.user.userId,
-		food: foodId,
-	};
+    // escape tags to prevent XSS
+    const newReview = {
+        rating,
+        title,
+        comment,
+        user: req.user.userId,
+        food: foodId,
+    };
 
-	const review = await Review.create(newReview);
-	res.status(200).json({ review });
+    const review = await Review.create(newReview);
+    res.status(200).json({ review });
 };
 
 const getSingleFoodReviews = async (req, res) => {
-	let {
-		params: { id: foodId },
-		query: { next_cursor },
-	} = req;
-	const queryObject = { food: foodId };
+    let {
+        params: { id: foodId },
+        query: { next_cursor },
+    } = req;
+    const queryObject = { food: foodId };
 
-	const resultsLimitPerLoading = 4;
-	if (next_cursor) {
-		const [rating, createdAt] = Buffer.from(next_cursor, "base64")
-			.toString("ascii")
-			.split("_");
+    const resultsLimitPerLoading = 4;
+    if (next_cursor) {
+        const [rating, createdAt, _id] = Buffer.from(next_cursor, "base64")
+            .toString("ascii")
+            .split("_");
 
-		queryObject.$or = [
-			{ rating: { $lt: rating } },
-			{ rating: rating, createdAt: { $lt: createdAt } }
-		];
-	}
+        queryObject.$or = [
+            { rating: { $lt: rating } },
+            { rating: rating, createdAt: { $lte: createdAt } },
+            { createdAt: createdAt, _id: { $ne: _id } },
+        ];
+    }
 
-	let reviews = Review.find(queryObject).populate({
-		path: "user",
-		select: "-_id username", // select username and not include _id
-	});
+    let reviews = Review.find(queryObject).populate({
+        path: "user",
+        select: "-_id username", // select username and not include _id
+    });
 
-	reviews = reviews.sort("-rating -createdAt");
-	reviews = reviews.limit(resultsLimitPerLoading);
-	const results = await reviews;
+    reviews = reviews.sort("-rating -createdAt");
+    reviews = reviews.limit(resultsLimitPerLoading);
+    const results = await reviews;
 
-	const count = await Review.countDocuments(queryObject);
-	const remainingResults = count - results.length;
-	next_cursor = null;
-	if (results.length !== count) {
-		const lastReview = results[results.length - 1];
-		next_cursor = Buffer.from(
-			lastReview.rating + "_" + lastReview.createdAt.toISOString()
-		).toString("base64");
-	}
+    const count = await Review.countDocuments(queryObject);
+    const remainingResults = count - results.length;
+    next_cursor = null;
+    if (results.length !== count) {
+        const lastReview = results[results.length - 1];
+        next_cursor = Buffer.from(
+            lastReview.rating +
+                "_" +
+                lastReview.createdAt.toISOString() +
+                "_" +
+                lastReview._id
+        ).toString("base64");
+    }
 
-	res.status(StatusCodes.OK).json({
-		results,
-		remainingResults,
-		next_cursor,
-	});
+    res.status(StatusCodes.OK).json({
+        results,
+        remainingResults,
+        next_cursor,
+    });
 };
 
 const updateReview = async (req, res) => {
-	const {
-		params: { id: reviewId },
-		user: { userId },
-	} = req;
-	const { rating, title, comment } = req.body;
+    const {
+        params: { id: reviewId },
+        user: { userId },
+    } = req;
+    const { rating, title, comment } = req.body;
 
-	if (title.match(regex) || comment.match(regex)) {
-		throw new CustomError.BadRequestError(
-			"This title or comment must not have strange characters"
-		);
-	}
+    if (title.match(regex) || comment.match(regex)) {
+        throw new CustomError.BadRequestError(
+            "This title or comment must not have strange characters"
+        );
+    }
 
-	const review = await Review.findOne({ _id: reviewId, user: userId });
-	if (!review) {
-		throw new CustomError.BadRequestError(
-			`Review with id ${reviewId} does not exist or this user does not write this review`
-		);
-	}
+    const review = await Review.findOne({ _id: reviewId, user: userId });
+    if (!review) {
+        throw new CustomError.BadRequestError(
+            `Review with id ${reviewId} does not exist or this user does not write this review`
+        );
+    }
 
-	review.rating = rating;
-	review.title = title;
-	review.comment = comment;
+    review.rating = rating;
+    review.title = title;
+    review.comment = comment;
 
-	await review.save();
-	res.status(200).json({ review });
+    await review.save();
+    res.status(200).json({ review });
 };
 
 const deleteReview = async (req, res) => {
-	const {
-		params: { id: reviewId },
-		user: { userId },
-	} = req;
+    const {
+        params: { id: reviewId },
+        user: { userId },
+    } = req;
 
-	const review = await Review.findOne({ _id: reviewId, user: userId });
-	if (!review) {
-		throw new CustomError.BadRequestError(
-			`Review with id ${reviewId} does not exist or this user does not write this review`
-		);
-	}
+    const review = await Review.findOne({ _id: reviewId, user: userId });
+    if (!review) {
+        throw new CustomError.BadRequestError(
+            `Review with id ${reviewId} does not exist or this user does not write this review`
+        );
+    }
 
-	await review.remove();
-	res.status(StatusCodes.OK).json({ msg: "Success! Review removed!" });
+    await review.remove();
+    res.status(StatusCodes.OK).json({ msg: "Success! Review removed!" });
 };
 
 module.exports = {
-	createReview,
-	getSingleFoodReviews,
-	updateReview,
-	deleteReview,
+    createReview,
+    getSingleFoodReviews,
+    updateReview,
+    deleteReview,
 };
