02e616d7fa53c4537685ce8793cc5e3bba1790eb
TuanDao2002 <tuan09092002@gmail.com>
2022-08-22 10:23:53 +0700

    update populate.js

--- populate.js
+++ populate.js
@@ -5,17 +5,29 @@ const Food = require("./models/Food");
 
 const jsonProducts = require("./food.json");
 
+const {
+    createAllAttributeSets,
+    findSimilar,
+} = require("./computation/index");
+
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI);
-        await Food.deleteMany();
-        await Food.create(jsonProducts);
-        console.log("Success!!!!");
-        process.exit(0);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
+	try {
+		await connectDB(process.env.MONGO_URI);
+		await Food.deleteMany();
+		await Food.create(jsonProducts);
+
+		const allFoods = await Food.find();
+		const allAttributeSets = await createAllAttributeSets(allFoods);
+		for (eachFood of allFoods) {
+			findSimilar(eachFood, allAttributeSets);
+		}
+
+		console.log("Success!!!!");
+		process.exit(0);
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
 };
 
 start();
